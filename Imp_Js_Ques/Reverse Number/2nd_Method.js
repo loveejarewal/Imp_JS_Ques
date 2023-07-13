c95ecdcba73b55e7ebce9aceab174d0d334a2397
Lovee <97103853+loveejarewal@users.noreply.github.com>
2023-07-13 17:58:37 +0530

    Delete Imp_Js_Ques/Reverse Number directory

--- Imp_Js_Ques/Reverse Number/2nd_Method.js
+++
@@ -1,7 +0,0 @@
-// converting the number to string
-
-
-let result = 0;
-let number = 78945;
-result = Number(String(number).split('').reverse().join(''));
-console.log('Reversed Number is :'+ result)
--- Imp_Js_Ques/Reverse Number/3rd_Method.js
+++
@@ -1,4 +0,0 @@
-// using arrow function
-
-const reverseNumber = number => parseFloat(number.toString().split('').reverse().join(''))* Math.sign(number);
-console.log(reverseNumber(78945))
